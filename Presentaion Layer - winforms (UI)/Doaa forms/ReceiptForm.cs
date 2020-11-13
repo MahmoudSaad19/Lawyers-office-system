f7ed4303d0342063112a2fef52ef53e41aaed14b
doaa <44727792+DoaaAhmad1@users.noreply.github.com>
2020-11-13 21:52:46 +0200

    Payment forms code

---
+++ Presentaion Layer - winforms (UI)/Doaa forms/ReceiptForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentaion_Layer___winforms__UI_
+{
+    public partial class ReceiptForm : Form
+    {
+        public ReceiptForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ReceiptForm_Load(object sender, EventArgs e)
+        {
+
+            this.TopMost = true;
+
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            HomePage home = FormPool.HomePage;
+            home.Show();
+            this.Hide();
+        }
+    }
+}
